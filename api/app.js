a017752438c2d6c9ee5eee83957a1d6c722f8cbd
John Olatubosun <toluolatubosun@gmail.com>
2022-08-07 21:24:57 +0100

    initial

---
+++ api/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+      "id":"1",
+      "title":"Book Review: The Name of the Wind"
+    },
+    {
+      "id":"2",
+      "title":"Game Review: Pokemon Brillian Diamond"
+    },
+    {
+      "id":"3",
+      "title":"Show Review: Alice in Borderland"
+    },
+    {
+      "id":"4",
+      "title":"Series Review: The Walking Dead"
+    },
+    {
+      "id":"5",
+      "title":"Movie Review: The Shawshank Redemption"
+    },
+  ])
+})
+
+app.listen(4000, () => {
+  console.log('listening for requests on port 4000')
+})
